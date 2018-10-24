4d93cb5bc825da02b0e3c2b94a383313282fa23d
novranfaathir <44427665+novranfaathir@users.noreply.github.com>
2018-10-24 15:26:20 +0700

    Mengganti mekanisme pengambilan data

--- infogempa.bash
+++ infogempa.bash
@@ -2,7 +2,7 @@
 # Dibuat oleh komunitas Open Source dan Keamanan Komputer
 # https://zerobyte.id/
 
-VERSI="1.0";
+VERSI="1.2";
 
 if [[ $1 == "--version" ]];then
 	echo "Versi $VERSI";
@@ -21,18 +21,18 @@ if [[ -z $(command -v column) ]];then
 	exit;
 fi
 
-echo "--,----------------------,-------,------,---------,---------,---------------------" > GEMPAINFO.TEMP
-echo "No,Waktu,Lintang,Bujur,Magnitudo,Kedalaman,Wilayah" >> GEMPAINFO.TEMP
-echo "--,----------------------,-------,------,---------,---------,---------------------" >> GEMPAINFO.TEMP
-curl -s http://www.bmkg.go.id/gempabumi/gempabumi-terkini.bmkg | grep -A8 '<tr>' | sed -e "s/[[:space:]]\+/ /g" | sed 's/--//g' | sed ':a;N;$!ba;s/\n/ /g' | sed 's/<\/tr>/\n/g' | sed 's/   //g' | sed 's/^ //g' | sed 's/<\/td><td>/,/g' | sed 's/<tr><td>//g' | sed 's/<\/td>//g' | sed 's/<br>//g' | sed '/^\s*$/d' >> GEMPAINFO.TEMP
-if [[ $(cat GEMPAINFO.TEMP | wc -l) -le 3 ]];then
+echo "--,----------------------,-------,------,---------,---------,---------------------" > /tmp/INFOGEMPA.TEMP
+echo "No,Waktu,Lintang,Bujur,Magnitudo,Kedalaman,Wilayah" >> /tmp/INFOGEMPA.TEMP
+echo "--,----------------------,-------,------,---------,---------,---------------------" >> /tmp/INFOGEMPA.TEMP
+curl -s http://www.bmkg.go.id/gempabumi/gempabumi-terkini.bmkg | grep -A8 '<tr>' | sed -e "s/[[:space:]]\+/ /g" | sed 's/--//g' | sed ':a;N;$!ba;s/\n/ /g' | sed 's/<\/tr>/\n/g' | sed 's/   //g' | sed 's/^ //g' | sed 's/<\/td><td>/,/g' | sed 's/<tr><td>//g' | sed 's/<\/td>//g' | sed 's/<br>//g' | sed '/^\s*$/d' >> /tmp/INFOGEMPA.TEMP
+if [[ $(cat /tmp/INFOGEMPA.TEMP | wc -l) -le 3 ]];then
 	echo "error: tolong periksa koneksi internet anda.";
-	rm GEMPAINFO.TEMP
+	rm /tmp/INFOGEMPA.TEMP
 	exit;
 fi
 echo "";
 echo " * INFORMASI GEMPA BUMI TERKINI";
 echo " * Badan Meteorologi Klimatologi dan Geofisika";
 echo "";
-cat GEMPAINFO.TEMP | sed -e 's/^/| /' -e 's/,/,| /g' -e 's/$/,|/' | grep -v 'Waktu Gempa' | column -t -s,
-rm GEMPAINFO.TEMP
+cat /tmp/INFOGEMPA.TEMP | sed -e 's/^/| /' -e 's/,/,| /g' -e 's/$/,|/' | grep -v 'Waktu Gempa' | column -t -s,
+rm /tmp/INFOGEMPA.TEMP
