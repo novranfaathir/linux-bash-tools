57267fb40d9f5d3fd3dfa98eab118cba86b8eeef
Novran Faathir <44427665+panophan@users.noreply.github.com>
2018-11-13 13:15:29 +0700

    Update setup.bash

--- setup.bash
+++ setup.bash
@@ -1,15 +1,16 @@
 #!/bin/bash
+# Setup :
+# git clone https://github.com/panophan/infogempa-linux.git
 # cd infogempa-linux/
 # chmod +x setup.bash
 # ./setup.bash
-
 
 mkdir /opt/infogempa-linux 2> /tmp/infogempa.log
 touch /opt/infogempa-linux/infogempa.bash 2> /tmp/infogempa.log
 cat infogempa.bash > /opt/infogempa-linux/infogempa.bash 2> /tmp/infogempa.log
 chmod 755 /opt/infogempa-linux/infogempa.bash 2> /tmp/infogempa.log
-ln -s /opt/infogempa-linux/infogempa.bash /usr/bin/infogempa 2> /tmp/infogempa.log
-ln -s /opt/infogempa-linux/infogempa.bash /usr/local/bin/infogempa 2> /tmp/infogempa.log
+ln -sf /opt/infogempa-linux/infogempa.bash /usr/bin/infogempa 2> /tmp/infogempa.log
+ln -sf /opt/infogempa-linux/infogempa.bash /usr/local/bin/infogempa 2> /tmp/infogempa.log
 
 if [[ -z /tmp/infogempa.log ]];
 then
@@ -20,4 +21,5 @@ else
 	echo "--- LOG ERROR ---"
 	cat /tmp/infogempa.log
 	echo "-----------------"
+	rm /tmp/infogempa.log
 fi
